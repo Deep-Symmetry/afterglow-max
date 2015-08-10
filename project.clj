ac9b17355c478eb0f1372cb6033ed74a0f0071e8
James Elliott <james.elliott@singlewire.com>
2015-08-09 22:33:06 -0500

    Prepare for release.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject afterglow-max "0.1.1-SNAPSHOT"
+(defproject afterglow-max "0.1.1"
   :description "A package for hosting Afterglow inside Cycling ‘74’s Max."
   :url "https://github.com/brunchboy/afterglow-max"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [afterglow "0.1.2-SNAPSHOT"]
+                 [afterglow "0.1.2"]
                  [com.taoensso/timbre "4.1.0"]]
   :uberjar-name "afterglow-max.jar"
   :manifest {"Name" ~#(str (clojure.string/replace (:group %) "." "/")
