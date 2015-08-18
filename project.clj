fb7842c17c75311013764482ae23999b70f69cc2
James Elliott <james.elliott@singlewire.com>
2015-08-17 22:07:22 -0500

    Explain QXF translation, prepare for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject afterglow-max "0.1.3-SNAPSHOT"
+(defproject afterglow-max "0.1.3"
   :description "A package for hosting Afterglow inside Cycling ‘74’s Max."
   :url "https://github.com/brunchboy/afterglow-max"
   :license {:name "Eclipse Public License"
