ac1d664c1f43306a9387c24d33c8597e3bc1bb98
James Elliott <james@deepsymmetry.org>
2015-09-29 18:34:08 -0500

    Release for Afterglow 0.1.4.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject afterglow-max "0.1.3"
+(defproject afterglow-max "0.1.4"
   :description "A package for hosting Afterglow inside Cycling ‘74’s Max."
   :url "https://github.com/brunchboy/afterglow-max"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [afterglow "0.1.3"]
+                 [afterglow "0.1.4"]
                  [com.taoensso/timbre "4.1.1"]]
   :main afterglow.core  ; So command-line arguments can be used, e.g. to convert QXF files.
   :uberjar-name "afterglow-max.jar"
