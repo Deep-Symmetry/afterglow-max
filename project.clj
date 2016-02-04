58d74442af7ed97983027179ce2806d4e409b547
James Elliott <james@deepsymmetry.org>
2016-02-04 09:58:07 -0600

    Claypoole exclusion no longer needed.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject afterglow-max "0.2.0"
+(defproject afterglow-max "0.2.1-SNAPSHOT"
   :description "A package for hosting Afterglow inside Cycling ‘74’s Max."
   :url "https://github.com/brunchboy/afterglow-max"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.climate/claypoole "1.1.0"]
-                 [afterglow "0.2.0" :exclusions [com.climate/claypoole]]
+                 [afterglow "0.2.1-SNAPSHOT"]
                  [com.taoensso/timbre "4.2.1"]]
   :main afterglow.core  ; So command-line arguments can be used, e.g. to convert QXF files.
   :uberjar-name "afterglow-max.jar"
