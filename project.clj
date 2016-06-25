e312a954755c1c908aaee3b5fff6ef4a1c3abc7a
James Elliott <james@deepsymmetry.org>
2016-06-25 11:27:50 -0500

    Update dependencies, track Afterglow version.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject afterglow-max "0.2.2-SNAPSHOT"
+(defproject afterglow-max "0.2.4-SNAPSHOT"
   :description "A package for hosting Afterglow inside Cycling ‘74’s Max."
   :url "https://github.com/brunchboy/afterglow-max"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [afterglow "0.2.3"]
+                 [afterglow "0.2.4-SNAPSHOT"]
                  [com.taoensso/timbre "4.4.0"]]
   :main afterglow.core  ; So command-line arguments can be used, e.g. to convert QXF files.
   :uberjar-name "afterglow-max.jar"
